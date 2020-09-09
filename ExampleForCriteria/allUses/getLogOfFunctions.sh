476bda721746957e4b50c2db1be542c4272b1cdb
Stefan <stefan.winzinger@uni-bamberg.de>
2020-09-09 15:17:21 +0200

    Fix use cases for example

--- ExampleForCriteria/allUses/getLogOfFunctions.sh
+++ ExampleForCriteria/allUses/getLogOfFunctions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-serverless logs -f Caller_AD --startTime 2h >> logs.txt
-serverless logs -f Callee1_AD --startTime 2h >> logs.txt
-serverless logs -f Callee2_AD --startTime 2h >> logs.txt
-serverless logs -f Getter1_AD --startTime 2h >> logs.txt
-serverless logs -f Getter2_AD --startTime 2h >> logs.txt
-serverless logs -f Writer_AD --startTime 2h >> logs.txt
+serverless logs -f Caller_AU --startTime 2h >> logs.txt
+serverless logs -f Callee1_AU --startTime 2h >> logs.txt
+serverless logs -f Callee2_AU --startTime 2h >> logs.txt
+serverless logs -f Getter1_AU --startTime 2h >> logs.txt
+serverless logs -f Getter2_AU --startTime 2h >> logs.txt
+serverless logs -f Writer_AU --startTime 2h >> logs.txt
