40704693ad4d7e9e97395d0a30ec220171bd523b
Stefan <stefan.winzinger@uni-bamberg.de>
2020-09-11 15:08:29 +0200

    Update Examples

---
+++ ExampleForCriteria/allDefs/Deleter_AD.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+
+exports.handler = (event, context, callback) => {
+    var params = {
+        Key: {
+            "myID": {
+                N: "1"
+            }
+        },
+        TableName: "Table_AD"
+    };
+    if (event.myID != undefined) {
+        params.Key.myID.N = event.myID;
+    }
+    dynamodb.deleteItem(params, function(err, data) {
+       console.log(data);
+       if (err) {
+            console.log(err, err.stack);
+            const response = {
+                statusCode: '500',
+                body: err
+            };
+            callback(null, response);
+        }
+        else {
+            const response = {
+                statusCode: '200',
+                body: 'Data deleted '
+            };
+            callback(null, response);
+        }
+    });
+};
--- ExampleForCriteria/allDefs/getLogOfFunctions.sh
+++ ExampleForCriteria/allDefs/getLogOfFunctions.sh
@@ -5,3 +5,4 @@ serverless logs -f Callee2_AD --startTime 2h >> logs.txt
 serverless logs -f Getter1_AD --startTime 2h >> logs.txt
 serverless logs -f Getter2_AD --startTime 2h >> logs.txt
 serverless logs -f Writer_AD --startTime 2h >> logs.txt
+serverless logs -f Deleter_AD --startTime 2h >> logs.txt
