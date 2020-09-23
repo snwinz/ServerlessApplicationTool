fc107745a587e5b7cbef74525c144c591e179e66
Stefan <stefan.winzinger@uni-bamberg.de>
2020-09-23 13:15:01 +0200

    Add preselection of file selection dialogue

--- ServerlessApplicationTool/src/main/java/gui/controller/LogFileSelectionController.java
+++ ServerlessApplicationTool/src/main/java/gui/controller/LogFileSelectionController.java
@@ -1,5 +1,13 @@
 package gui.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
 import gui.view.CoverageResultView;
 import gui.view.LogFileSelectionView;
 import javafx.event.ActionEvent;
@@ -8,67 +16,67 @@ import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import model.logic.LogEvaluation.LogFileEvaluator;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class LogFileSelectionController {
-    private final FileChooser fileChooser = new FileChooser();
+	private final FileChooser fileChooser = new FileChooser();
 
-    @FXML
-    TextField logFilePath;
-    private LogFileSelectionView view;
+	@FXML
+	TextField logFilePath;
+	private LogFileSelectionView view;
 
-    public void setViewAndSetup(LogFileSelectionView view) {
-        this.view = view;
-        setDefaultPaths();
-    }
+	public void setViewAndSetup(LogFileSelectionView view) {
+		this.view = view;
+		setDefaultPaths();
+	}
 
-    private void setDefaultPaths() {
-        Properties loadProps = new Properties();
-        try {
-            loadProps.loadFromXML(new FileInputStream("settings.xml"));
-            String logFilePathText = loadProps.getProperty("logFilePath");
-            logFilePath.setText(logFilePathText);
-        } catch (
-                IOException e) {
-        }
-    }
+	private void setDefaultPaths() {
+		Properties loadProps = new Properties();
+		try {
+			loadProps.loadFromXML(new FileInputStream("settings.xml"));
+			String logFilePathText = loadProps.getProperty("logFilePath");
+			logFilePath.setText(logFilePathText);
+		} catch (IOException e) {
+		}
+	}
 
-    public void evaluateCoverage(ActionEvent actionEvent) {
-        savePathsAsDefault();
-        LogFileEvaluator evaluator = new LogFileEvaluator();
-        String result = evaluator.evaluate(logFilePath.getText());
+	@FXML
+	public void evaluateCoverage(ActionEvent actionEvent) {
+		savePathsAsDefault();
+		LogFileEvaluator evaluator = new LogFileEvaluator();
+		String result = evaluator.evaluate(logFilePath.getText());
 
-        CoverageResultView view = new CoverageResultView();
+		CoverageResultView view = new CoverageResultView();
 
-        view.show(result);
+		view.show(result);
 
-    }
+	}
 
-    public void chooselogFilePath(ActionEvent actionEvent) {
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-        File file = fileChooser.showOpenDialog(view.getStage());
-        if (file != null) {
-            logFilePath.setText(file.getPath());
-        }
+	@FXML
+	public void chooselogFilePath(ActionEvent actionEvent) {
+		String pathOfInput = logFilePath.getText();
+		Path selectedFile = Path.of(pathOfInput);
+		if (Files.exists(selectedFile)) {
+			fileChooser.setInitialDirectory(selectedFile.toAbsolutePath().getParent().toFile());
+		} else {
+			fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+		}
+		File file = fileChooser.showOpenDialog(view.getStage());
+		if (file != null) {
+			logFilePath.setText(file.getPath());
+		}
 
-    }
+	}
 
+	private void savePathsAsDefault() {
+		Properties saveProps = new Properties();
+		try {
+			saveProps.loadFromXML(new FileInputStream("settings.xml"));
+			saveProps.setProperty("logFilePath", logFilePath.getText());
+			saveProps.storeToXML(new FileOutputStream("settings.xml"), "");
+		} catch (IOException e) {
+			System.err.println("Default paths couldn't be saved!");
+			e.printStackTrace();
+		}
 
-    private void savePathsAsDefault() {
-        Properties saveProps = new Properties();
-        try {
-            saveProps.loadFromXML(new FileInputStream("settings.xml"));
-            saveProps.setProperty("logFilePath", logFilePath.getText());
-            saveProps.storeToXML(new FileOutputStream("settings.xml"), "");
-        } catch (IOException e) {
-            System.err.println("Default paths couldn't be saved!");
-            e.printStackTrace();
-        }
-
-    }
+	}
 
 }
